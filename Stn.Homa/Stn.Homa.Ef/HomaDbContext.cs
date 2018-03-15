9e9275d474ebc923cfd3da59ccb61d8a68ac4471
Sebastian <sebastian.nasuta@gmail.com>
2018-03-15 21:49:02 +0100

    new project - WebApi + EF

---
+++ Stn.Homa/Stn.Homa.Ef/HomaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stn.Homa.Ef
+{
+    public class HomaDbContext : DbContext
+    {
+        public HomaDbContext(DbContextOptions<HomaDbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
